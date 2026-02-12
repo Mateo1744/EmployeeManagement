db431d27aecf4867a93652eff59ef8a24efa983f
Mateo Naranjo <mateonaranjo43@gmail.com>
2026-02-12 14:24:04 -0500

    Add Employee Model

---
+++ Models/Employee.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement.Models
+{
+    internal class Employee
+    {
+        public int Id { get; set; }
+
+        public string FullName { get; set; }
+
+        public decimal Salary { get; set; }
+
+        public string Status { get; set; }
+
+        public DateTime HireDate { get; set; }
+
+        public DateTime? EndDate { get; set; }
+    }
+}
