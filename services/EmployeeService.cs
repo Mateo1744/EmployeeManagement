42d8061cd0c2e9f0242628b0b8d65284f120c557
Mateo Naranjo <mateonaranjo43@gmail.com>
2026-02-12 14:31:49 -0500

    Add EmployeeService with employee list

---
+++ services/EmployeeService.cs
@@ -0,0 +1,19 @@
+﻿using EmployeeManagement.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement.Services
+{
+    public class EmployeeService
+    {
+        private readonly List<Employee> employees;
+
+        public EmployeeService()
+        {
+            employees = new List<Employee>();
+        }
+    }
+}
